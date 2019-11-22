2615bf26aa42c82ca6bcace91ceace1e03b3edea
PeterLenting <pplenting@gmail.com>
2019-11-22 12:53:31 +0000

    Testing Django 4

--- todo/test_views.py
+++ todo/test_views.py
@@ -8,12 +8,12 @@ class TestViews(TestCase):
         page = self.client.get("/")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "todo_list.html")
-    
+
     def test_get_add_item_page(self):
         page = self.client.get("/add")
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "item_form.html")
-    
+
     def test_get_edit_item_page(self):
         item = Item(name="Create a Test")
         item.save()
@@ -21,7 +21,7 @@ class TestViews(TestCase):
         page = self.client.get("/edit/{0}".format(item.id))
         self.assertEqual(page.status_code, 200)
         self.assertTemplateUsed(page, "item_form.html")
-    
+
     def test_get_edit_page_for_item_that_does_not_exist(self):
         page = self.client.get("/edit/1")
         self.assertEqual(page.status_code, 404)
